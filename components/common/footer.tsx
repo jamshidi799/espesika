82407673d7ee511a56d3b4fcd8573dd39e35aa52
jamshidi.m799 <jamshidi.m799@gmail.com>
2020-12-16 16:17:47 +0330

    fix footer links

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="container-fluid bg__footer p-5">
@@ -5,19 +7,29 @@ export default function Footer() {
         <div className="col-12 col-sm-12 col-md-4 text-right mt-4">
           <h1 className="">اسپه‌ سیکا</h1>
           <p className="">
-            اسپه سیکا (در زبان مازندرانی یعنی اردک سفید) یک مجموعه پرواربندی و پرورش قوغاز و بوقلمون برنز آمریکایی به صورت ارگانیک هست که
-            چهار سال سابقه فعالیت دارد . برای خرید جوجه غاز و جوجه بوقلمون با ما تماس بگیرید
+            اسپه سیکا (در زبان مازندرانی یعنی اردک سفید) یک مجموعه پرواربندی و
+            پرورش قوغاز و بوقلمون برنز آمریکایی به صورت ارگانیک هست که چهار سال
+            سابقه فعالیت دارد . برای خرید جوجه غاز و جوجه بوقلمون با ما تماس
+            بگیرید
           </p>
         </div>
         <div className="col-12 col-sm-12 col-md-4 text-right mt-4">
           <h1 className="">اطلاعات تماس</h1>
           <div>
             <div className="d-flex align-items-center">
-              <img src="img/icons/phone.png" className="icon icon-phone" alt="phone" />
+              <img
+                src="img/icons/phone.png"
+                className="icon icon-phone"
+                alt="phone"
+              />
               <div className="">7639 929 0991</div>
             </div>
             <div className="d-flex align-items-center">
-              <img src="img/icons/location.png" className="icon" alt="location" />
+              <img
+                src="img/icons/location.png"
+                className="icon"
+                alt="location"
+              />
               <div className="">مازندران,محمودآباد,روستای‌آزادمون</div>
             </div>
             <div className="d-flex align-items-center">
@@ -29,26 +41,30 @@ export default function Footer() {
         <div className="col-12 col-sm-12 col-md-4 text-right mt-4">
           <h1>محصولات</h1>
           <div>
-            <a href="/ghooghaz.html" className="d-flex align-items-center">
-              <img src="img/icons/arrow.png" className="icon" alt="email" />
-              <h5>غاز‌اوکراینی</h5>
-            </a>
-            <a href="/boghy.html" className="d-flex align-items-center">
-              <img src="img/icons/arrow.png" className="icon" alt="email" />
-              <h5>بوقلمون آمریکایی</h5>
-            </a>
-            <a href="/ghazmahli.html" className="d-flex align-items-center">
-              <img src="img/icons/arrow.png" className="icon" alt="email" />
-              <h5>غاز‌محلی</h5>
-            </a>
-            <a href="/ordak.html" className="d-flex align-items-center">
-              <img src="img/icons/arrow.png" className="icon" alt="email" />
-              <h5>اردک محلی</h5>
-            </a>
-            <a href="/" className="d-flex align-items-center">
-              <img src="img/icons/arrow.png" className="icon" alt="email" />
-              <h5>جوجه محلی</h5>
-            </a>
+            <Link href="/ghooghaz">
+              <a className="d-flex align-items-center">
+                <img src="img/icons/arrow.png" className="icon" alt="email" />
+                <h5>غاز‌اوکراینی</h5>
+              </a>
+            </Link>
+            <Link href="/boghy">
+              <a className="d-flex align-items-center">
+                <img src="img/icons/arrow.png" className="icon" alt="email" />
+                <h5>بوقلمون آمریکایی</h5>
+              </a>
+            </Link>
+            <Link href="/ghazmahali">
+              <a className="d-flex align-items-center">
+                <img src="img/icons/arrow.png" className="icon" alt="email" />
+                <h5>غاز‌محلی</h5>
+              </a>
+            </Link>
+            <Link href="/ordak">
+              <a className="d-flex align-items-center">
+                <img src="img/icons/arrow.png" className="icon" alt="email" />
+                <h5>اردک محلی</h5>
+              </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -60,7 +76,7 @@ export default function Footer() {
         }
 
         .bg__footer::before {
-          content: '';
+          content: "";
           display: block;
           position: absolute;
           left: 0;
